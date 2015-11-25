a5914a10efef3e32c9b0a8d1727d678495f71b8a
Michał Herda <phoe@openmailbox.org>
2015-11-25 03:20:13 +0100

    does it look like I did everything I wanted?

--- package.lisp
+++ package.lisp
@@ -1,40 +1,16 @@
 ;;;; package.lisp
 
 (defpackage #:pseudonyms
-  (:use #:cl #:trivial-garbage)
+  (:use #:cl
+	#:trivial-garbage
+	#:named-readtables)
   (:export :*pseudonym-table*
+	   :set-pseudonym-macro-character 
 	   :defpseudonym
 	   :pmakunbound
 	   :print-pseudonyms
 	   :pseudonym-reader)
   (:documentation
-   "============================ PSEUDONYMS ===================================
+   "=== PSEUDONYMS by phoe ===
 
-I found that Lisp nicknames, as defined in CLHS, have a few problems that I will count here.
-
-1) They are not changeable without internal side-effects. RENAME-PACKAGE is destructive, as it
-kills off any previous names the package.
-2) They collide. Nickname GL is used by at least three different Lisp packages.
-
-The solution I provide here is a different approach to nicknames that does not use any of the
-original nickname code, as defined in CLHS.
-
-Pseudonyms, in opposition to nicknames, can be defined by the user inside one's code, like
-this:
-> (defpseudonym \"longpackagename\" \"lpn\")
-And removed like this:
-> (pmakunbound \"lpn\") ;; OR (pmakunbound \"longpackagename\")
-
-From within the code, one can refer to a pseudonymized package this way:
-> $lpn:something
-A reader macro will automatically translate it to its normal version of
-longpackagename:something. This is usable both within the REPL and within usual code.
-
-All pseudonyms are local to the current package: for instance, pseudonyms defined within
-CL-USER are not usable anywhere outside the CL-USER package.
-
-An utility function print-pseudonyms will print all pseudonyms for a given package. If not
-supplied a package name as an argument, it will print all pseudonyms for current package (as
-shown by the *package* global variable).
-
-"))
+See attached README.md for documentation."))
